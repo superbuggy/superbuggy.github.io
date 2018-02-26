268c34e383500994b9f5198cc42a3a8d71f6a134
superbuggy <jpqreichard@gmail.com>
2018-02-26 15:37:55 -0500

    remove invalid value for alpha

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ let forestDivs = [...document.querySelectorAll('.forest')]
 let heartSpans = [...document.querySelectorAll('.heart')]
 
 forestDivs.forEach((forestDiv, i) => {
-  const alpha = .6 / forestDivs.length * i
+  const alpha = 0.6 / forestDivs.length * i
   const heighty = forestDiv.parentElement.clientHeight / forestDivs.length
   const rowHeight = heighty * (i + 1) * 1.5
   const randomOffset = parseInt(Math.random() * 10) + 1
   const leftPostion = (randomOffset + 90) / (forestDivs.length - i) + randomOffset
-  forestDiv.style.left=`${leftPostion}vw`
+  forestDiv.style.left = `${leftPostion}vw`
   forestDiv.style.color = `rgba(0, 0, 0, ${alpha})`
-  forestDiv.style.background = `rgba(0, 0, 0, 0})`
+  forestDiv.style.background = `rgba(0, 0, 0, 0)`
   forestDiv.style.top = `${rowHeight}px`
   forestDiv.style.zIndex = -i
 })
 
 heartSpans.forEach(heartSpan => {
   const hover = ({target, type, ...event}) => {
-    target.style.textShadow = type === 'mouseout' 
+    target.style.textShadow = type === 'mouseout'
       ? ''
       : '-2px -2px 12px rgba(58, 126, 58, .9), 2px 2px 12px rgba(255, 156, 0, .9)'
   }
