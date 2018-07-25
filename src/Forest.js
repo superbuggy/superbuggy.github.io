472bb8108aeee4eb68a8315ff3f247eb65517683
James Reichard <jpqreichard@gmail.com>
2018-07-25 11:36:16 -0400

    Create Forest context component for Trees

---
+++ src/Forest.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Forest = props => {
+  return (
+    <svg
+      viewBox={`0 0 ${window.innerWidth} ${window.innerHeight}`}
+    >
+      {props.children}
+    </svg>
+  )
+}
