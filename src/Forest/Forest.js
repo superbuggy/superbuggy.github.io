2bc20ea96e67f3a433f5da007ec81e9ccbd0a9c4
James Reichard <jpqreichard@gmail.com>
2018-07-25 13:45:56 -0400

    Center trees

--- src/Forest/Forest.js
+++ src/Forest/Forest.js
@@ -4,7 +4,7 @@ import { Tree } from '../Tree/Tree'
 export const Forest = props => {
   const TREE_SIZE = 100
   const TREE_SPACING_MODIFIER = .66
-  const VARIANCE = 33
+  const VARIANCE = Math.round(TREE_SIZE - TREE_SIZE * TREE_SPACING_MODIFIER)
   
   const TREES_PER_ROW = Math.round(window.innerWidth / (TREE_SIZE * TREE_SPACING_MODIFIER))
   const ROWS = Math.round(window.innerHeight / (TREE_SIZE * TREE_SPACING_MODIFIER))
@@ -12,11 +12,11 @@ export const Forest = props => {
   function rowOfTrees (treesPerRow, y, rowNumber) {
     return [...Array(treesPerRow).keys()].map(index => {
       const SPACE_BETWEEN_TREES = window.innerWidth / TREES_PER_ROW
-      
+      const CENTERING_OFFSET = -Math.round(VARIANCE / 2)
       return (
         <Tree 
           key={index}
-          x={index * SPACE_BETWEEN_TREES + rowNumber * VARIANCE}
+          x={index * SPACE_BETWEEN_TREES + rowNumber * VARIANCE + CENTERING_OFFSET}
           y={y}
           size={TREE_SIZE}
         />
@@ -26,7 +26,7 @@ export const Forest = props => {
   
   
   function forest (rows) {
-    const SPACE_BETWEEN_ROWS = window.innerHeight / ROWS
+    const SPACE_BETWEEN_ROWS = window.innerHeight / rows
     return [...Array(rows).keys()].map(index => {
       return rowOfTrees(TREES_PER_ROW - index, index * SPACE_BETWEEN_ROWS, index)
     })
