19ea3f30ad46562c6b9c1891098976b7f74be818
superbuggy <jpqreichard@gmail.com>
2023-10-24 16:37:10 -0400

    ♻️ Restructure stylesheets with SCSS

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
+import './stylesheets/main.css'
+import './stylesheets/main.scss'
 import App from './App.vue'
 import {router} from './router'
 
